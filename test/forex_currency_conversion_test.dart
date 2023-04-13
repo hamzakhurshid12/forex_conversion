f5bc156704279d2ac3131ba2fb58d1408ebaa161
Hamza Khurshid <hamzaandumar@gmail.com>
2023-04-13 10:16:02 +0500

    Fixes typo in test case

--- test/forex_currency_conversion_test.dart
+++ test/forex_currency_conversion_test.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
-import '../lib/forex_conversion.dart';
+import 'package:forex_conversion/forex_conversion.dart';
 
 void main() {
-  test('adds one to input values', () async {
+  test('Checks price of USD with itself', () async {
     final fx = Forex();
     final response = await fx.getAllCurrenciesPrices();
     expect(response["USD"], 1.0);
